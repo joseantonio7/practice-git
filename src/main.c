d2f3ce1db29c96e47b81530487c76b3b25a9080e
bunshichi <bunshichi777@gmail.com>
2021-07-05 14:08:06 -0400

    agregados comentarios

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "fib.h"
 #include "fac.h"
  
+//comernto algo 
 int main(){
 	int i = 0;
 	for (i=0; i<4 ;i++){
@@ -10,5 +11,6 @@ int main(){
 	printf("El elemento 10 de la serie de fibonacci es: %ld\n",fibonacci(10));
 	printf("El factorial de 5 es: %ld\n",factorial(5));
 
+	//y aquí tambien
 	return 0;
 }
